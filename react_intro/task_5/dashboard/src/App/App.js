fb07199282439a32f58cbf4bdab2f6455a288b1c
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-05-06 20:36:17 -0500

    task 9

--- react_intro/task_5/dashboard/src/App/App.js
+++ react_intro/task_5/dashboard/src/App/App.js
@@ -1,31 +1,29 @@
-import React from "react";
-import logo from '../assets/atlas_logo.png';
-import "./App.css";
-import { getFooterCopy, getFullYear } from "../utils/utils";
+import React from 'react';
+import './App.css';
+import logo from '../assets/holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from '../utils/utils';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="atlas_logo" alt="logo" />
+    <>
+      <header className='App-header'>
+        <img src={logo} alt='logo' />
         <h1>School dashboard</h1>
       </header>
-
-      <div className="App-body">
+      <main role='main' className='App-body'>
         <p>Login to access the full dashboard</p>
-        <form>
-          <label htmlFor="email">Email: </label>
-          <input type="email" id="email" name="email" />
-          <label htmlFor="password">Password: </label>
-          <input type="password" id="password" name="password" />
-          <button>OK</button>
-        </form>
-      </div>
-
-      <footer className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)} </p>
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' id='email' />
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password' id='password' />
+        <button type='button'>OK</button>
+      </main>
+      <footer className='App-footer'>
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
       </footer>
-    </div>
+    </>
   );
 }
 
--- react_intro/task_5/dashboard/src/App/App.test.js
+++ react_intro/task_5/dashboard/src/App/App.test.js
@@ -1,25 +1,19 @@
-// App.test.js
-import React from "react";
-import { shallow } from "enzyme";
-import App from "./App";
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
 
-describe("App", () => {
-    it("renders without crashing", () => {
-        shallow(<App />);
-    });
+const wrapper = shallow(<App />);
 
-    it("renders a div with the class App-header", () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(".App-header").exists()).toBe(true);
-    });
+it('renders without crashing', () => {
+  shallow(<App />);
+});
 
-    it("renders a div with the class App-body", () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(".App-body").exists()).toBe(true);
-    });
-
-    it("renders a div with the class App-footer", () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(".App-footer").exists()).toBe(true);
-    });
+it('renders App-header', () => {
+  expect(wrapper.find('header.App-header').exists()).toEqual(true);
+});
+it('renders App-body', () => {
+  expect(wrapper.find('main.App-body').exists()).toEqual(true);
+});
+it('renders App-footer', () => {
+  expect(wrapper.find('footer.App-footer').exists()).toEqual(true);
 });
