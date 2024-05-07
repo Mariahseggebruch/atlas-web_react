6dc38ae06f4f3d97242ce280bd33784f8ef6555e
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-05-06 20:29:17 -0500

    updated the webpack configruation

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,46 +1,45 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, '../src/index.js'),
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "../dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('./dist'),
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve('./dist'),
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
             },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-                use: [
-                    "file-loader",
-                    {
-                        loader: "image-webpack-loader",
-                    },
-                ],
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"],
-                    },
-                },
-            },
+          },
         ],
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, '../dist'),
+      },
+      {
+        test: /\.jsx?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        compress: true,
-        hot: true,
-    },
-    devtool: "inline-source-map",
-    };
+      },
+    ],
+  },
+};
